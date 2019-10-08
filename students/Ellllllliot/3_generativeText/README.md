3f170b67ea2ce618c6dc459b665691b987408c47
elliotallard <eallard@andrew.cmu.edu>
2019-10-07 21:58:03 -0400

    Add files via upload

---
+++ students/Ellllllliot/3_generativeText/README.md
@@ -0,0 +1,25 @@
+ ELLIOT, YOU TEXT LIKE A 4 YEAR OLD
+
+
+ For the class Data Gardens, I created a generative text-er. Input a message you want to send or have been sent in the "SPEak to me" box & press enter - and I can infuse/transform it to say something I would text at this stage of my life, October 7th, 2019.
+
+ This piece uses text messages that I have sent to others - selected to be silly and anonymous. The text is generated through a modified n-gram algorithm, taken from Dan Shiffman. 
+
+ This piece was inspired by multiple lines of thought - 
+
+ 	1) my friend texted me that after being in a conversation with me, I laughed out loud.
+
+ 	2) the meaning & language (?) of emoji? How does emoji change the nature of texting, conversation, literature? Is there such thing as emoji syntax? What do emojis do to tone of voice? How do they add to the virtual, the artifice?
+
+ 	3) our attention and behaviors are chopped up & condensed by mediums such as text, IM, instagram, Facebook, and smartphones in general. What is it like to create poetry that mimics a fleeting attention span? What tactics exist within this limited attention span? 
+
+
+ Reach out to me - elliot.x.allard@gmail.com
+
+
+
+
+Comments - 
+
+	October 7th, 2019:
+	This is when I presented it. I tried to style it differently, aggregating the outputs of editor and displaying them. But took to long, will work on it ~eventually~
---
+++ students/Ellllllliot/3_generativeText/myTexts.js
@@ -0,0 +1,83 @@
+// This file takes some of my most recent imessages
+// which are anonymized. Taken Monday, October 7th, 2019
+// and spits them out into an array, seperated by spaces.
+
+let rawTexts = ["like all of my lovers should thank her, she is doing work for them",
+				"This is Me  getting feelings for someone and getting into a super toxic relationship",
+				"But only for us too",
+				"A group photo",
+				"We need to add to this 👿👿👿",
+				"Literally you are learning all of this in ixds1",
+				"Lm aoooooo",
+				"Sa weet",
+				"If u want",
+				"（⌒_⌒）",
+				"OMG yes",
+				"Awww 😘 what do you have cookin this week!",
+				"Hehehehe. If you supply the scanner",
+				"All of the best parts of an office job",
+				"Lit a candle, getting into an inspiration mood",
+				"Still going hehe",
+				"Yes!!! Rest rest rest",
+				"Ok ty lol in bed",
+				"How many attempts you got? Will determine if I open my laptop",
+				"But I already answered it and took the L",
+				"Fffff is that the rover one?",
+				"And .25 of a pomegranate. Taking care of ourselves",
+				"She was Born to take selfies with you!",
+				"When u can't take your own advice 😔",
+				"Bees are happiness!",
+				"Wait you just found that!! 😱",
+				"Precisely baby nosey",
+				"pic makes it hard to tell",
+				"How are you doing! I also have gossip but it's personal so it's gossip rant but it's at the point where it is gossip cuz I'm not emotional about it",
+				"??? gossip = non-emotional? Idk",
+				"Hehehe get it",
+				"Miss you and your cheek kisses 💋",
+				"Let’s fooooooo",
+				"Same top Long sleeves",
+				"Where is u",
+				"😛😛😛 We have that weekend off 😈😈😈",
+				"Lmao I’m so happy I can gift you this moment",
+				"Hello there :) Cousin Elliot texting you ✌️✌️",
+				"😭 keeping up with tradition. I haven’t drunk wine .... in ... so long",
+				"Hey QT! Let's go to Phipps this weekend",
+				"OMG what is it",
+				"Cute :D",
+				"¯\_(ツ)_/¯",
+				"join if/when",
+				"YOU JUST WALKED INTO MY CLASS Lmaoooooo",
+				"Please react with a ❤️ to this message if you plan on coming 😃",
+				"Don’t worry about that, I’m taking care of it!",
+				"Dw about it, I figured you were saying you had some shit go wrong 😢",
+				"Elliot Allard",
+				"You are like flash",
+				"Actually! I think I'm good",
+				"Looking at old Pictures and 🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯🤯",
+				"Or friendship is in the stars",
+				"🧚‍♂️",
+				"🌱🌻🌟 You have a wonderful & restful rest of your Sunday, busy gal",
+				"Awwww 😿",
+				"Oh I so would’ve! I was just finishing up a homework, and have class now. Hope you’re outside it’s beautifULL",
+				"Omg “I cost $5 per text”",
+				"“Market price”",
+				"So his eyebrows are genetic",
+				"His eyebrows are 😵😵😵😵",
+				"Emailed person",
+				"Let me know when you start heading to campus, I might move. I get fidgety",
+				"I didn't have that god of a timeeee :( "
+
+
+
+				]
+
+
+
+let myTexts = []
+
+for (var i=0; i < rawTexts.length; i++) {
+	myTexts[i] = rawTexts[i].split(" ")
+}
+	
+
+
